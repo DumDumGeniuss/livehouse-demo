e36d883f51873c071dd1b932147313dee9f5828a
DumDumGeniuss <messi_yang@syntrontech.com>
2016-12-13 01:31:36 +0800

    新增圖片上團API

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match '*any' => 'application#options', :via => [:options]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'hello#index'
+  resources :files
 end
